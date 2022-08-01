38063a558eb269236ae89d27cc56513020e9e8ac
Jerry Yu <jeyu54217.gmail.com>
2022-08-01 18:10:23 +0800

    init

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt" // format package
+// standard lib provide different packages
+// packages = collection of source files
+// entrypoint, only have one
+func main() {
+	fmt.Print("test")
+
+}
