03afa4e903029b3b9f0769a117c6987be86dff2a
jeyu54217 <73396926+jeyu54217@users.noreply.github.com>
2022-08-04 17:32:42 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
-import "fmt" // format package
-// standard lib provide different packages
-// packages = collection of source files
-// entrypoint, only have one
-func main() {
-	fmt.Print("test")
+import ("fmt" // format package
+	"context"
+        "github.com/line/line-bot-sdk-go/v7/linebot"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+        // standard lib provide different packages
+        // packages = collection of source files
+        // entrypoint, only have one
+       )
 
+
+// Connection URI
+const uri = "mongodb://user:pass@sample.host:27017/?maxPoolSize=20&w=majority"
+
+func main() {
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	// Ping the primary
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
 }
